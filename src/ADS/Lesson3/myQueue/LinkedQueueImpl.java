bc9e46ac6c4a309025eed61f51d42db55764104f
Kseniia Novozhilova <nowozhilowa-ksenia@mail.ru>
2020-11-05 02:44:24 +0300

    hm4

---
+++ src/ADS/Lesson3/myQueue/LinkedQueueImpl.java
@@ -0,0 +1,39 @@
+package ADS.Lesson3.myQueue;
+
+import ADS.Lesson4.TwoSideLinkedList;
+import ADS.Lesson4.TwoSideLinkedListImpl;
+
+public class LinkedQueueImpl<E> implements Queue<E> {
+
+    private TwoSideLinkedList<E> data;
+
+    public LinkedQueueImpl() {
+        this.data = new TwoSideLinkedListImpl<>();
+    }
+
+    @Override
+    public boolean insert(E value) {
+        data.insertLast(value);
+        return true;
+    }
+
+    @Override
+    public E remove() {
+        return data.removeFirst();
+    }
+
+    @Override
+    public E peekHead() {
+        return data.getFirst();
+    }
+
+    @Override
+    public int size() {
+        return data.size();
+    }
+
+    @Override
+    public boolean isFull() {
+        return false;
+    }
+}
